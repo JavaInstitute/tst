dbcbd8c3539c50b96f14b7741fc5f2a052dd1ec4
Ishara-PC <Ishara-PC@Ishara>
2015-09-01 17:16:31 +0530

    this is 1st comment

--- jiat/src/jiat/Jiat.java
+++ jiat/src/jiat/Jiat.java
@@ -16,7 +16,8 @@ public class Jiat {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here\
+        System.out.println("yes");
     }
     
 }
